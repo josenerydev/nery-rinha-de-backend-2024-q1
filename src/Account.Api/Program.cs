4bfec50f9825332f2c9187f8630953af10e8908f
Jose Nery <josenerydev@gmail.com>
2024-02-29 16:19:48 -0300

    removendo yarp

--- src/Account.Api/Program.cs
+++ src/Account.Api/Program.cs
@@ -8,24 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AccountApiContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("AccountApiContext") ?? throw new InvalidOperationException("Connection string 'AccountApiContext' not found.")));
 
-// Add services to the container.
 builder.Services.AddScoped<TransacaoService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
 
 app.MapControllers();
 
