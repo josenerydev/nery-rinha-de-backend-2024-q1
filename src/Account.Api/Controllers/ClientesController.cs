3cb1d363b005273bc5df32e5d1481788d4967153
Jose Nery <josenerydev@gmail.com>
2024-03-01 16:30:58 -0300

    .

--- src/Account.Api/Controllers/ClientesController.cs
+++ src/Account.Api/Controllers/ClientesController.cs
@@ -3,6 +3,8 @@ using Account.Api.Services;
 
 using Microsoft.AspNetCore.Mvc;
 
+using RedLockNet;
+
 namespace Account.Api.Controllers
 {
     [Route("[controller]")]
@@ -11,43 +13,59 @@ namespace Account.Api.Controllers
     {
         private readonly ITransacaoService _transacaoService;
         private readonly ITransacaoValidacaoService _transacaoValidacaoService;
+        private readonly IDistributedLockFactory _lockFactory;
 
-        public ClientesController(ITransacaoService transacaoService, ITransacaoValidacaoService transacaoValidacaoService)
+        public ClientesController(ITransacaoService transacaoService, ITransacaoValidacaoService transacaoValidacaoService, IDistributedLockFactory lockFactory)
         {
             _transacaoService = transacaoService;
             _transacaoValidacaoService = transacaoValidacaoService;
+            _lockFactory = lockFactory;
         }
 
         [HttpPost("{id}/transacoes")]
         public async Task<IActionResult> PostTransacao(int id, TransacaoRequisicaoDto transacaoRequisicaoDTO)
         {
-            var tipoValido = _transacaoValidacaoService.ValidarTipo(transacaoRequisicaoDTO.Tipo);
-            if (!tipoValido.IsValid)
-                return UnprocessableEntity(tipoValido.ErrorMessage);
+            string chaveBloqueio = $"transacao:{id}";
 
-            var valorValido = _transacaoValidacaoService.ValidarValor(transacaoRequisicaoDTO.Valor);
-            if (!valorValido.IsValid)
-                return UnprocessableEntity(valorValido.ErrorMessage);
+            await using var _lock = await _lockFactory.CreateLockAsync(
+                chaveBloqueio,
+                TimeSpan.FromSeconds(10),
+                TimeSpan.FromSeconds(3),
+                TimeSpan.FromSeconds(1)
+            );
 
-            var descricaoValida = _transacaoValidacaoService.ValidarDescricao(transacaoRequisicaoDTO.Descricao);
-            if (!descricaoValida.IsValid)
-                return UnprocessableEntity(descricaoValida.ErrorMessage);
-
-            (bool sucesso, var cliente, string erro) = await _transacaoService.RealizarTransacao(id, transacaoRequisicaoDTO.Valor, transacaoRequisicaoDTO.Tipo, transacaoRequisicaoDTO.Descricao);
-
-            if (!sucesso)
+            if (_lock.IsAcquired)
             {
-                if (cliente == null)
+                var tipoValido = _transacaoValidacaoService.ValidarTipo(transacaoRequisicaoDTO.Tipo);
+                if (!tipoValido.IsValid)
+                    return UnprocessableEntity(tipoValido.ErrorMessage);
+
+                var valorValido = _transacaoValidacaoService.ValidarValor(transacaoRequisicaoDTO.Valor);
+                if (!valorValido.IsValid)
+                    return UnprocessableEntity(valorValido.ErrorMessage);
+
+                var descricaoValida = _transacaoValidacaoService.ValidarDescricao(transacaoRequisicaoDTO.Descricao);
+                if (!descricaoValida.IsValid)
+                    return UnprocessableEntity(descricaoValida.ErrorMessage);
+
+                (bool sucesso, var cliente, string erro) = await _transacaoService.RealizarTransacao(id, transacaoRequisicaoDTO.Valor, transacaoRequisicaoDTO.Tipo, transacaoRequisicaoDTO.Descricao);
+
+                if (!sucesso)
                 {
-                    return NotFound("Cliente não encontrado.");
+                    if (cliente == null)
+                    {
+                        return NotFound("Cliente não encontrado.");
+                    }
+                    else if (erro == "Saldo insuficiente.")
+                    {
+                        return UnprocessableEntity("Transação excede o limite do cliente.");
+                    }
                 }
-                else if (erro == "Saldo insuficiente.")
-                {
-                    return UnprocessableEntity("Transação excede o limite do cliente.");
-                }
+
+                return Ok(new TransacaoRespostaDto { Limite = cliente.Limite, Saldo = cliente.Saldo });
             }
 
-            return Ok(new TransacaoRespostaDto { Limite = cliente.Limite, Saldo = cliente.Saldo });
+            return StatusCode(StatusCodes.Status423Locked, "O recurso está temporariamente bloqueado por outra operação.");
         }
 
         [HttpGet("{id}/extrato")]
--- src/Account.Api/Program.cs
+++ src/Account.Api/Program.cs
@@ -3,13 +3,30 @@ using Account.Api.Services;
 
 using Microsoft.EntityFrameworkCore;
 
+using RedLockNet;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AccountApiContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("AccountApiContext") ?? throw new InvalidOperationException("Connection string 'AccountApiContext' not found.")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AccountApiContext")
+        ?? throw new InvalidOperationException("Connection string 'AccountApiContext' not found.")));
 
 builder.Services.AddScoped<ITransacaoService, TransacaoService>();
 builder.Services.AddScoped<ITransacaoValidacaoService, TransacaoValidacaoService>();
+
+var configurationOptions = ConfigurationOptions.Parse(builder.Configuration["REDIS_HOST"]!);
+
+var multiplexer = new List<RedLockMultiplexer> {
+    ConnectionMultiplexer.Connect(configurationOptions)
+};
+
+var redlockFactory = RedLockFactory.Create(multiplexer);
+
+builder.Services.AddSingleton<IDistributedLockFactory>(redlockFactory);
 
 builder.Services.AddControllers();
 
